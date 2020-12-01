7f498c94170e30dc070c2a0277bfee538ec3ab5e
Matt Tennison <matt@matttennison.com>
2020-12-01 20:35:30 +0000

    Complete Day 1
    I tried to optimise the first for performance, by putting it in a hash
    table. This avoided nested for loops, and made the algorithm O(n).
    
    Then I immediately ran into an obstacle with the second part - I
    couldn't see a better solution than O(n**2) - by adding every combo
    of two up, and feeding it to the first solution. When attempting this
    I found out about .combination, which basically solved the problem
    for me - so I went with that.

---
+++ lib/day_one.rb
@@ -0,0 +1,16 @@
+class DayOne
+  def solve(arr)
+    lookup = Hash.new(false)
+
+    arr.each {|x| lookup[x] = true }
+    match = arr.find {|x| lookup[2020 - x] }
+
+    match == nil ? -1 : match * (2020 - match)
+  end
+
+  def solve_second(arr)
+    arr.combination(3) do |combos|
+      return combos[0] * combos[1] * combos[2] if combos.sum === 2020
+    end
+  end
+end
---
+++ spec/day_one_spec.rb
@@ -0,0 +1,23 @@
+require "day_one"
+
+RSpec.describe DayOne do
+  describe 'solve' do
+    it 'returns a solution given array with two valid numbers' do
+      expect(DayOne.new.solve([1721, 299])).to eq(514579)
+    end
+
+    it 'returns -1 given array without two valid numbers' do
+      expect(DayOne.new.solve([1, 2])).to eq(-1)
+    end
+
+    it 'returns a solution given longer array with two valid numbers' do
+      expect(DayOne.new.solve([2, 1721, 299])).to eq(514579)
+    end
+  end
+
+  describe 'solve_second' do
+    it 'returns a solution given array with three valid numbers' do
+      expect(DayOne.new.solve_second([979, 366, 675])).to eq(241861950)
+    end
+  end
+end
