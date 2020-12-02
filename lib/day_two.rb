5941e73676cc3cf462c35775829c1de1f5ee56a8
Matt Tennison <matt@matttennison.com>
2020-12-02 20:42:58 +0000

    Complete Day Two
    This one seemed more straightforward - once you parsed the input it
    was some fairly straightforward logic to work out the answer.
    
    I like the method_name? syntax for methods that return booleans - very
    neat. arr.count is pretty handy too.

---
+++ lib/day_two.rb
@@ -0,0 +1,16 @@
+class DayTwo
+  def solve(arr)
+    arr.count do |entry|
+      minCharacterCount, maxCharacterCount, character, password = /(\d+)-(\d+) (\w): (\w+)/.match(entry).captures
+      numberOfCharacterInPassword = password.count(character)
+      numberOfCharacterInPassword.between?(minCharacterCount.to_i, maxCharacterCount.to_i)
+    end
+  end
+
+  def solve_two(arr)
+    arr.count do |entry|
+      firstPosition, secondPosition, character, password = /(\d+)-(\d+) (\w): (\w+)/.match(entry).captures
+      password[firstPosition.to_i - 1].eql?(character) ^ password[secondPosition.to_i - 1].eql?(character)
+    end
+  end
+end
---
+++ spec/day_two_spec.rb
@@ -0,0 +1,32 @@
+require "day_two"
+
+RSpec.describe DayTwo do
+  describe 'solve' do
+    it 'returns 1 given one valid password' do
+      input = ['1-3 a: abcde', '1-3 b: cdefg']
+      expect(DayTwo.new.solve(input)).to eq(1)
+    end
+
+    it 'returns 2 given two valid passwords' do
+      input = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
+      expect(DayTwo.new.solve(input)).to eq(2)
+    end
+
+    it 'handles very large passwords' do
+      input = ['1-3 a: abcde', '1-3 b: cdefg', '2-11 c: ccccccccccc']
+      expect(DayTwo.new.solve(input)).to eq(2)
+    end
+  end
+
+  describe 'solve_two' do
+    it 'returns 1 given one valid password' do
+      input = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
+      expect(DayTwo.new.solve_two(input)).to eq(1)
+    end
+
+    it 'returns 2 given two valid passwords' do
+      input = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: cdccccccc']
+      expect(DayTwo.new.solve_two(input)).to eq(2)
+    end
+  end
+end
