fbf227bf6d824b7f55c7c7c7136c56cef7b7aeef
Matt Tennison <matt@matttennison.com>
2020-12-03 22:36:46 +0000

    Add Day Three solution
    This required a bit more thinking. Started with a for loop but wanted
    to keep using these all these handy array methods, so found a way

---
+++ lib/day_three.rb
@@ -0,0 +1,20 @@
+class DayThree
+  def initialize(input)
+    @input = input
+  end
+
+  def solve(right_delta_per_turn = 3, down_delta_per_turn = 1)
+    grid = @input.split("\n").map do |row|
+      row.chars.map{|char| char.eql?("#")}
+    end
+
+    rows_we_hit = grid.each_slice(down_delta_per_turn).map(&:first)
+
+    has_hit_tree_on_row = rows_we_hit.map.with_index do |row, index|
+      x = index * right_delta_per_turn
+      row[x % row.count]
+    end
+
+    has_hit_tree_on_row.count(true)
+  end
+end
---
+++ spec/day_three_spec.rb
@@ -0,0 +1,23 @@
+require "day_three"
+
+RSpec.describe DayThree do
+  describe 'solve' do
+    where(:input, :right_delta_per_turn, :down_delta_per_turn, :answer) do
+      [
+        [".##\n###\n###", 1, 1, 2],
+        [".##\n###\n###", 1, 2, 1],
+        ["...\n...\n...", 1, 1, 0],
+        ["...\n###\n...", 1, 2, 0],
+        [".##\n.#.\n..#", 1, 1, 2]
+      ]
+    end
+
+    with_them do
+      describe "in a #{params[:right_delta_per_turn]}x#{params[:down_delta_per_turn]} toboggan" do
+        it "hits #{params[:answer]} trees when going down #{params[:input]} " do
+          expect(DayThree.new(input).solve(right_delta_per_turn, down_delta_per_turn)).to eq(answer)
+        end
+      end
+    end
+  end
+end
