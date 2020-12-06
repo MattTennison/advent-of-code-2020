38d940d851de01d1b20b145042e85c96168a9ba1
Matt Tennison <matt@matttennison.com>
2020-12-06 12:32:53 +0000

    Solve Day 5
    I tried to go for more of a OO-style here, breaking the parts into
    domain objects like BoardingPass and Flight. Turning the binary
    choices into seat_ids was harder than I thought - I might've been
    better going for something recursive.

---
+++ lib/day_five.rb
@@ -0,0 +1,45 @@
+class BoardingPass
+  def initialize(pass)
+    @pass = pass
+  end
+
+  def pass
+    @pass
+  end
+
+  def seat_id
+    (row * 8) + seat
+  end
+
+  private
+
+  def row 
+    row_data_in_pass = @pass.slice(0, 7)
+    row = row_data_in_pass.chars.reduce(Range.new(0, 127)) do |acc, character|
+      mid_point = acc.min + (acc.size / 2)
+      character.eql?("F") ? Range.new(acc.min, mid_point - 1) : Range.new(mid_point, acc.max)
+    end.first
+  end
+
+  def seat
+    seat_data_in_pass = @pass.slice(7, 3)
+    seat = seat_data_in_pass.chars.reduce(Range.new(0, 7)) do |acc, character|
+      mid_point = acc.min + (acc.size / 2)
+      character.eql?("L") ? Range.new(acc.min, mid_point - 1) : Range.new(mid_point, acc.max)
+    end.first
+  end
+end
+
+class Flight
+  def initialize(boarding_passes)
+    @boarding_passes = boarding_passes
+  end
+
+  def missing_seat_ids
+    max_row = 127
+    max_seat = 8
+    max_seat_id = 127 * 8 + 8
+  
+    Set.new(1...max_seat_id) - @boarding_passes.map{|p| p.seat_id}
+  end
+end
---
+++ spec/day_five_spec.rb
@@ -0,0 +1,40 @@
+require "day_five"
+
+RSpec.describe BoardingPass do
+  describe "BoardingPass" do
+    where(:pass, :expected_seat_id) do
+      [
+        ['FBFBBFFRLR', 357],
+        ['BFFFBBFRRR', 567],
+        ['FFFBBBFRRR', 119],
+        ['BBFFBBFRLL', 820]
+      ]
+    end
+
+    with_them do
+      it "gives pass #{params[:pass]} the seat ID #{params[:expected_seat_id]}" do
+        boarding_pass = BoardingPass.new(pass)
+        expect(boarding_pass.seat_id).to eq(expected_seat_id)
+      end
+    end
+  end
+end
+
+RSpec.describe Flight do
+  describe "Flight" do
+    where(:present_passes, :missing_seat_id) do
+      [
+        [["FBFBBFFRLR", "FBFBBFFRRR"], 358],
+      ]
+    end
+
+    with_them do
+      it "identifies #{params[:missing_seat_id]} as a missing seat ID" do
+        boarding_passes = present_passes.map{|pass| BoardingPass.new(pass)}
+        flight = Flight.new(boarding_passes)
+
+        expect(flight.missing_seat_ids).to include(missing_seat_id)
+      end
+    end
+  end
+end
