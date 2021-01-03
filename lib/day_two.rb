6106c725973f9195f2f21fa300161ce710144193
Matt Tennison <matt@matttennison.com>
2021-01-03 23:32:50 +0000

    Fix lib/day_two.rb issues

--- lib/day_two.rb
+++ lib/day_two.rb
@@ -3,16 +3,16 @@
 class DayTwo
   def solve(arr)
     arr.count do |entry|
-      minCharacterCount, maxCharacterCount, character, password = /(\d+)-(\d+) (\w): (\w+)/.match(entry).captures
-      numberOfCharacterInPassword = password.count(character)
-      numberOfCharacterInPassword.between?(minCharacterCount.to_i, maxCharacterCount.to_i)
+      min_character_count, max_character_count, character, password = /(\d+)-(\d+) (\w): (\w+)/.match(entry).captures
+      no_of_characters_in_password = password.count(character)
+      no_of_characters_in_password.between?(min_character_count.to_i, max_character_count.to_i)
     end
   end
 
   def solve_two(arr)
     arr.count do |entry|
-      firstPosition, secondPosition, character, password = /(\d+)-(\d+) (\w): (\w+)/.match(entry).captures
-      password[firstPosition.to_i - 1].eql?(character) ^ password[secondPosition.to_i - 1].eql?(character)
+      first_position, second_position, character, password = /(\d+)-(\d+) (\w): (\w+)/.match(entry).captures
+      password[first_position.to_i - 1].eql?(character) ^ password[second_position.to_i - 1].eql?(character)
     end
   end
 end
